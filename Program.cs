304fbb997fb1eb5c9165eec22c0787164d456ac4
midnight33 <gra33@bk.ru>
2023-03-02 17:55:11 +0300

    Задача 1 семинар 2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное писло:");
+try
+{
+    int Value = Convert.ToInt32(Console.ReadLine());
+    int Length = Value.ToString().Length;
+    if (Length == 3)
+    {
+        int result = (Value / 10) % 10;
+        Console.WriteLine($"Вторая цифра:  {result}");
+    }
+    else
+    {
+        Console.WriteLine("Не трехзначное писло");
+    }
+}
+catch (System.Threading.ThreadAbortException)
+{
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
